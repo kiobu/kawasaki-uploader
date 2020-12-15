6a7eba360437d596b5d43f89bb879c30c4f4e839
kiobu <cdolson30@gmail.com>
2020-12-14 21:04:51 -0500

    Fix some premature returns.

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ if (!process.argv[2]) {
     return console.log("No file was given.")
 }
 
-let strbuild = `curl -v -X POST -F key='${conf.key}' -F file='@${process.argv[2]}' ${conf.remote}`
+let strbuild = `curl -k -o - -v -F key='${conf.key}' -F file='@${process.argv[2]}' ${conf.remote}`
 
 exec(strbuild, (nerror, stdout, stderr) => {
-    if (nerror) { return console.log(`error: ${nerror.message}`); }
-    if (stderr) { return console.log(`stderr: ${stderr}`); }
+    if (nerror) { console.log(`error: ${nerror.message}`); }
+    if (stderr) { console.log(`stderr: ${stderr}`); }
     console.log(`stdout: ${stdout}`);
 })
